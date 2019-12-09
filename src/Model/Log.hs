0ef154a7f05118100af43d89e6e2817c42842a79
Michael Lee <mlee@earnestresearch.com>
2019-12-09 02:32:00 -0500

    extended Log

--- src/Model/Log.hs
+++ src/Model/Log.hs
@@ -1,12 +1,14 @@
 module Model.Log where
 
 import Colog
-import Data.Text
+import Data.Text as T
 import Polysemy
 
 data Log m a where
   LogStdout :: Text -> Log m ()
   LogStderr :: Text -> Log m ()
+  LogStdout' :: [Text] -> Log m ()
+  LogStderr' :: [Text] -> Log m ()
 
 makeSem ''Log
 
@@ -14,6 +16,8 @@ logToIO :: Member (Embed IO) r => Sem (Log ': r) a -> Sem r a
 logToIO = interpret $ \case
   LogStdout x -> embed $ log' logTextStdout x
   LogStderr x -> embed $ log' logTextStderr x
+  LogStdout' xs -> embed $ log' logTextStdout $ T.unwords xs
+  LogStderr' xs -> embed $ log' logTextStderr $ T.unwords xs
   where
     log' :: LogAction IO Text -> Text -> IO ()
     log' logger msg = logger <& msg
