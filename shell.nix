6a57e0ed00facbf1fc24d2dfdedf1603e1928ffa
ml <metaml@gmail.com>
2020-06-12 06:39:49 -0400

    bug fix

--- shell.nix
+++ shell.nix
@@ -1,26 +1,24 @@
 { nixpkgs ? import ./nix/nix.nix }:
 let
   pkgs = import nixpkgs {};
-  hkgs = pkgs.haskellPackages;
-  ghc = pkgs.haskellPackages.ghcWithPackages (hpkgs: with hpkgs; [
-    cabal-install
-    zlib
-  ]);
-
+  hkg = pkgs.haskellPackages;
+  ghc = hkg.ghcWithPackages (hpkgs: with hpkgs; [ ghcide
+                                                  cabal-install
+                                                  zlib
+                                                ]
+                            );
 in
   with pkgs;
-
   mkShell {
-    buildInputs = [
-      binutils
-      git
-      gnumake
-      gnumake
-      hkgs.fswatcher
-      hkgs.ghc
-      hkgs.ghcid
-      hkgs.hlint
-      less
-      sourceHighlight
-    ];
+    buildInputs = [ binutils
+                    ghc
+                    git
+                    gnumake
+                    gnumake
+                    hkg.fswatcher
+                    hkg.ghcid
+                    hkg.hlint
+                    less
+                    sourceHighlight
+                  ];
   }
